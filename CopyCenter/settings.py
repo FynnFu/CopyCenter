7f7ecf5b72b49a9f3aa962779a76d2a6d0d77587
FynnFu <rasidaliev53@gmail.com>
2023-03-11 21:31:58 +0600

    ff

--- CopyCenter/settings.py
+++ CopyCenter/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+xa68ncn+y(p^(#0h(q$*=mr2p$m+(gn_1h+ru#ix5+of66%4p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
